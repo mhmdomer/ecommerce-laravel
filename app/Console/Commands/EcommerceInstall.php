55d3b2b154356c224415e142938db6c2c7a67900
m7md3omer <mohammedomer789@gmail.com>
2019-10-22 02:52:06 +0200

    added the option to force the artisan command without confirmation message

--- app/Console/Commands/EcommerceInstall.php
+++ app/Console/Commands/EcommerceInstall.php
@@ -38,43 +38,49 @@ class EcommerceInstall extends Command
      */
     public function handle()
     {
-        if($this->confirm('This will delete all your current data and database and install the default dummy data Are You Sure?')) {
-            File::deleteDirectory(public_path('storage/products/dummy'));
-            File::deleteDirectory(public_path('storage/users'));
-            $this->callSilent('storage:link');
-            $copySuccess1 = File::copyDirectory(public_path('/images/products'), public_path('storage/products/dummy'));
-            $copySuccess2 = File::copyDirectory(public_path('/images/users'), public_path('storage/users'));
-            if($copySuccess1 && $copySuccess2) {
-                $this->info('Images successfully moved to storage folder');
-            }
-            $this->call('migrate:refresh', [
-                '--seed' => true
-            ]);
-            $this->info('cleared the database and seeded basic tables successfully');
-            $this->call('db:seed', [
-                '--class' => 'VoyagerDatabaseSeeder'
-            ]);
-            $this->call('db:seed', [
-                '--class' => 'VoyagerDummyDatabaseSeeder'
-            ]);
-            $this->call('db:seed', [
-                '--class' => 'DataTypesTableSeederCustom'
-            ]);
-            $this->call('db:seed', [
-                '--class' => 'DataRowsTableSeederCustom'
-            ]);
-            $this->call('db:seed', [
-                '--class' => 'MenusTableSeederCustom'
-            ]);
-            $this->call('db:seed', [
-                '--class' => 'MenuItemsTableSeederCustom'
-            ]);
-            $this->call('db:seed', [
-                '--class' => 'PermissionsTableSeederCustom'
-                ]);
-            $this->call('db:seed', [
-                '--class' => 'PermissionRoleTableSeederCustom'
-            ]);
+        if($this->option('force')) {
+            $this->proceed();
+        } else if($this->confirm('This will delete all your current data and database and install the default dummy data Are You Sure?')) {
+            $this->proceed();
+        }            
+    }
+
+    protected function proceed() {
+        File::deleteDirectory(public_path('storage/products/dummy'));
+        File::deleteDirectory(public_path('storage/users'));
+        $this->callSilent('storage:link');
+        $copySuccess1 = File::copyDirectory(public_path('/images/products'), public_path('storage/products/dummy'));
+        $copySuccess2 = File::copyDirectory(public_path('/images/users'), public_path('storage/users'));
+        if($copySuccess1 && $copySuccess2) {
+            $this->info('Images successfully moved to storage folder');
         }
+        $this->call('migrate:refresh', [
+            '--seed' => true
+        ]);
+        $this->info('cleared the database and seeded basic tables successfully');
+        $this->call('db:seed', [
+            '--class' => 'VoyagerDatabaseSeeder'
+        ]);
+        $this->call('db:seed', [
+            '--class' => 'VoyagerDummyDatabaseSeeder'
+        ]);
+        $this->call('db:seed', [
+            '--class' => 'DataTypesTableSeederCustom'
+        ]);
+        $this->call('db:seed', [
+            '--class' => 'DataRowsTableSeederCustom'
+        ]);
+        $this->call('db:seed', [
+            '--class' => 'MenusTableSeederCustom'
+        ]);
+        $this->call('db:seed', [
+            '--class' => 'MenuItemsTableSeederCustom'
+        ]);
+        $this->call('db:seed', [
+            '--class' => 'PermissionsTableSeederCustom'
+            ]);
+        $this->call('db:seed', [
+            '--class' => 'PermissionRoleTableSeederCustom'
+        ]);
     }
 }
