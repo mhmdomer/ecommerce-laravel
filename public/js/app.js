f2ffb1c0ef3d510c489c782e7515ace2babec391
m7md3omer <mohammedomer789@gmail.com>
2019-11-10 14:17:56 +0200

    changed to new google font

--- public/js/app.js
+++ public/js/app.js
@@ -49540,8 +49540,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\ecommerce\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\ecommerce\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\xampp\htdocs\ecommerce\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\xampp\htdocs\ecommerce\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merienda One" rel="stylesheet">
     @yield('stylesheet')
 
     <!-- Styles -->
