9d5342641f40c87ba07440a63537ab62a0c7f2fd
m7md3omer <mohammedomer789@gmail.com>
2019-11-12 00:13:57 +0200

    heroku test, remove email sending

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -61,7 +61,7 @@ class CheckoutController extends Controller
             
             // SUCCESSFUL
             $this->decreaseQuantities();
-            Mail::to('me@me.com')->send(new OrderPlaced($order));
+            // Mail::to('me@me.com')->send(new OrderPlaced($order));
             Cart::instance('default')->destroy();
             session()->forget('coupon');
             return redirect()->route('welcome')->with('success', 'Your order is completed successfully!');
