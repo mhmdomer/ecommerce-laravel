f27b1c838eafc94602abed697a14e91b8aaa8ebf
m7md3omer <mohammedomer789@gmail.com>
2020-03-24 14:23:34 +0200

    heroku modifications

--- app/library/helper.php
+++ app/library/helper.php
@@ -1,7 +1,9 @@
 <?php
 
 function productImage($path) {
-    return $path && file_exists('storage/' . $path) ? asset('storage/' . $path) : asset('images/not-found.jpg');
+
+    return $path && file_exists('public/images/' . $path) ? asset('storage/' . $path) : asset('images/not-found.jpg');
+    // return $path && file_exists('storage/' . $path) ? asset('storage/' . $path) : asset('images/not-found.jpg');
 }
 
 function format($price) {
