ce1109ea9df504edebd8567ef08bc9daa937921e
mhmdomer <mohammedomer789@gmail.com>
2024-10-15 18:44:04 +0400

    upgrade laravel mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 window.$ = require('jquery');
 
 /**
